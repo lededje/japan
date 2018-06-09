e4498fed876e82c4c90d2d155c3cab14c0f43adf
Miles Spoor <git@likeminded.io>
2018-06-09 21:51:55 +0900

    Added headings
    Added water and rice

---
+++ app/components/Title/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './styles.css'
+
+const Title = ({ children }) => <div className={styles.title}>{children}</div>;
+
+export default Title
--- app/pages/index.js
+++ app/pages/index.js
@@ -5,6 +5,7 @@ import styles from './base.css'
 import Phrase from '../containers/Phrase'
 import LanguagePicker from '../containers/LanguagePicker'
 
+import Title from '../components/Title'
 import PhraseGroup from '../components/PhraseGroup'
 import PhraseSpacer from '../components/PhraseSpacer'
 import withRedux from '../components/withRedux'
@@ -16,23 +17,26 @@ class Index extends React.Component {
 
         <LanguagePicker />
 
+        <Title>Basics</Title>
+
         <PhraseGroup>
           <Phrase english="Hello" japanese="Kon'nichiwa" syllabary="こんにちは" />
           <Phrase english="Goodbye" japanese="Sayōnara" syllabary="さようなら" />
-          <PhraseSpacer />
-          <Phrase english="Thank you" japanese="Arigatō" syllabary="ありがとう" />
           <PhraseSpacer />
           <Phrase english="Yes" japanese="Hai" syllabary="はい" />
           <Phrase english="No" japanese="Īe" syllabary="いいえ" />
         </PhraseGroup>
 
+        <Title>Numbers</Title>
+
         <PhraseGroup>
-          <Phrase english="0" japanese="Zero" syllabary="〇" />
           <Phrase english="1" japanese="Ichi" syllabary="一" />
           <Phrase english="2" japanese="Ni" syllabary="二" />
           <Phrase english="3" japanese="San" syllabary="三" />
           <Phrase english="4" japanese="Shi" syllabary="四" />
           <Phrase english="5" japanese="Go" syllabary="五" />
+        </PhraseGroup>
+        <PhraseGroup>
           <Phrase english="6" japanese="Roku" syllabary="六" />
           <Phrase english="7" japanese="Shichi" syllabary="セ" />
           <Phrase english="8" japanese="Hachi" syllabary="八" />
@@ -40,6 +44,7 @@ class Index extends React.Component {
           <Phrase english="10" japanese="Juu" syllabary="十" />
         </PhraseGroup>
 
+        <Title>Transport</Title>
         <PhraseGroup>
           <Phrase english="Train" japanese="Ressha" syllabary="列車" />
           <Phrase english="Tram" japanese="Toramu" syllabary="トラム" />
@@ -47,16 +52,22 @@ class Index extends React.Component {
           <Phrase english="Station" japanese="Eki" syllabary="駅" />
         </PhraseGroup>
 
+        <Title>Foods</Title>
         <PhraseGroup>
           <Phrase english="Vegetable" japanese="Yasai" syllabary="野菜" />
           <Phrase english="Chicken" japanese="Chikin" syllabary="チキン" />
           <Phrase english="Beef" japanese="Gyūniku" syllabary="牛肉" />
           <Phrase english="Pork" japanese="Butaniku" syllabary="豚肉" />
+        </PhraseGroup>
+        <PhraseGroup>
           <Phrase english="Fish" japanese="Ressha" syllabary="魚" />
           <Phrase english="Duck" japanese="Ahiru" syllabary="アヒル" />
           <Phrase english="Shrimp" japanese="Ebi" syllabary="エビ" />
+          <Phrase english="Rice" japanese="Gohan" syllabary="ご飯" />
+          <Phrase english="Water" japanese="Mizu" syllabary="水" />
         </PhraseGroup>
 
+        <Title>Common Phrases</Title>
         <PhraseGroup>
           <Phrase english="I don't need a bag" japanese="Watashi wa baggu ga hitsuyō nai" syllabary="私はバッグが必要ない" />
         </PhraseGroup>
